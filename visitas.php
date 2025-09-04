2069d4dfaaf2ad67da0e5f3c0ac84b56322dbf66
Melquijava <melqueribeiroamorim@gmail.com>
2025-09-04 02:39:39 -0300

    Subindo PHP

---
+++ visitas.php
@@ -0,0 +1,22 @@
+<?php
+// Arquivo onde vamos salvar o número de visitas
+$arquivo = "contador.txt";
+
+// Se o arquivo não existir, cria com valor inicial 0
+if (!file_exists($arquivo)) {
+    file_put_contents($arquivo, "0");
+}
+
+// Lê o número atual
+$visitas = (int) file_get_contents($arquivo);
+
+// Soma +1
+$visitas++;
+
+// Salva de volta
+file_put_contents($arquivo, $visitas);
+
+// Retorna em JSON
+header('Content-Type: application/json');
+echo json_encode(["visitas" => $visitas]);
+?>
