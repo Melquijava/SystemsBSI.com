20125e59124eda398764f41e91af8f2554b8d8f5
melqueribeiroamorim@gmail.com <165730189+Melquijava@users.noreply.github.com>
2025-10-11 19:44:55 -0300

    Fix: Geral tirando PHP

--- systems-bsi-frontend/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Inicia o PHP-FPM em segundo plano (daemon)
-php-fpm -D
-
-# Inicia o Nginx em primeiro plano (foreground)
-# Este comando mantém o container rodando
-nginx -g "daemon off;"
